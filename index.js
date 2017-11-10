746f2646c8089fd5c2a1ac4abd1e3f6441fc1e94
Steve Anderson <nurfserd@gmail.com>
2017-11-10 00:42:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ if (false) {
 } else {
   console.log('This will get printed')
 }
-
 if (6 + 3 === 9) {
   console.log('Giraffes have no vocal cords.')
 }
-
 if (6 + 3 < 5) {
   console.log('The humingbird is the only animal that can fly backwards')
 }
+
 var dog = 'satisfied'
 
 if (dog === 'hungry') {
   console.log('Refilling food bowl.')
+} else if (dog === 'thirsty') {
+  console.log('Refilling water bowl.')
 } else {
   console.log('Reading newspaper.')
 }
+
